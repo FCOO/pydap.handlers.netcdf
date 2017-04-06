13605e2d870636e52b557a4a00ce79a546f47cd6
Jesper Baasch-Larsen <jla@fcoo.dk>
2017-04-06 15:46:11 +0200

    pydap 3.2.0 support + python3 support

--- src/pydap/handlers/netcdf/__init__.py
+++ src/pydap/handlers/netcdf/__init__.py
@@ -5,109 +5,195 @@ import re
 import time
 from stat import ST_MTIME
 from email.utils import formatdate
-
 import numpy as np
+
 from pkg_resources import get_distribution
 
-# available NetCDF modules
-NETCDF4 = "netCDF4"
-PUPYNERE = "pupynere"
-
-try:
-    from netCDF4 import Dataset as netcdf_file
-    NETCDF_MODULE = NETCDF4
-
-    def attrs(var):
-        """Use the netCDF4 API to load all attributes."""
-        if hasattr(var, "ncattrs"):
-            return {k: getattr(var, k) for k in var.ncattrs()}
-        else:
-            return {}
-
-except ImportError:
-    from pupynere import netcdf_file
-    NETCDF_MODULE = PUPYNERE
-    
-    def attrs(var):
-        """Pupynere stores attributes in a special attribute."""
-        return getattr(var, "_attributes", {})
-
-from pydap.model import *
+from pydap.model import DatasetType, GridType, BaseType
 from pydap.handlers.lib import BaseHandler
 from pydap.exceptions import OpenFileError
+
+# Python 2.6 compatibility:
+try:
+    from collections import OrderedDict
+except ImportError:
+    from ordereddict import OrderedDict
+
+# Check for netCDF4 presence:
+try:
+    from netCDF4 import Dataset as netcdf_file
+
+    def attrs(var):
+        return dict((k, getattr(var, k)) for k in var.ncattrs())
+except ImportError:
+    from scipy.io.netcdf import netcdf_file
+
+    def attrs(var):
+        return var._attributes
 
 
 class NetCDFHandler(BaseHandler):
 
-    """A simple handler for NetCDF files."""
+    """A simple handler for NetCDF files.
+    Here's a standard dataset for testing sequential data:
+    """
 
-    __version__ = get_distribution("pydap.handlers.netcdf").version
+    __version__ = get_distribution("pydap").version
     extensions = re.compile(r"^.*\.(nc|cdf)$", re.IGNORECASE)
 
     def __init__(self, filepath):
         BaseHandler.__init__(self)
 
+        self.filepath = filepath
         try:
-            self.fp = netcdf_file(filepath)
+            with netcdf_file(self.filepath, 'r') as source:
+                self.additional_headers.append(('Last-modified',
+                                               (formatdate(
+                                                time.mktime(
+                                                    time.localtime(
+                                                        os.stat(filepath)
+                                                        [ST_MTIME])
+                                                        )))))
+
+                # shortcuts
+                vars = source.variables
+                dims = source.dimensions
+
+                # build dataset
+                name = os.path.split(filepath)[1]
+                self.dataset = DatasetType(name,
+                                           attributes=dict(
+                                                      NC_GLOBAL=attrs(source)))
+                for dim in dims:
+                    if dims[dim] is None:
+                        self.dataset.attributes['DODS_EXTRA'] = {
+                            'Unlimited_Dimension': dim,
+                        }
+                        break
+
+                # add grids
+                grids = [var for var in vars if var not in dims]
+                for grid in grids:
+                    self.dataset[grid] = GridType(grid, attrs(vars[grid]))
+                    # add array
+                    self.dataset[grid][grid] = BaseType(grid,
+                                                        LazyVariable(
+                                                            source,
+                                                            grid,
+                                                            grid,
+                                                            self.filepath),
+                                                        vars[grid].dimensions,
+                                                        attrs(vars[grid]))
+                    # add maps
+                    for dim in vars[grid].dimensions:
+                        if dim not in vars:
+                            length = len(dims[dim])
+                            var = np.arange(length)
+                            attr = {}
+                        else:
+                            var = vars[dim][:]
+                            attr = attrs(vars[dim])
+                        self.dataset[grid][dim] = BaseType(dim, var,
+                                                           None,
+                                                           attr)
+
+                # add dims
+                for dim in dims:
+                    if dim not in vars:
+                        length = len(dims[dim])
+                        var = np.arange(length)
+                        attr = {}
+                    else:
+                        var = vars[dim][:]
+                        attr = attrs(vars[dim])
+                    self.dataset[dim] = BaseType(dim, var, None,
+                                                 attr)
         except Exception as exc:
             message = 'Unable to open file %s: %s' % (filepath, exc)
             raise OpenFileError(message)
 
-        self.additional_headers.append(
-            ('Last-modified', (
-                formatdate(
-                    time.mktime(
-                        time.localtime(os.stat(filepath)[ST_MTIME]))))))
 
-        # shortcuts
-        vars = dict(self.fp.variables)
-        dims = dict(self.fp.dimensions)
+class LazyVariable:
+    def __init__(self, source, name, path, filepath):
+        self.filepath = filepath
+        self.path = path
+        var = source[self.path]
+        self.dimensions = var._getdims()
+        self.dtype = np.dtype(var.dtype)
+        self.datatype = var.dtype
+        self.ndim = len(var.dimensions)
+        self._shape = var.shape
+        self._reshape = var.shape
+        self.scale = True
+        self.name = name
+        self.size = np.prod(self.shape)
+        self._attributes = dict((attr, var.getncattr(attr))
+                                for attr in var.ncattrs())
+        return
 
-        # turn off automatic scaling
-        if NETCDF_MODULE == NETCDF4:
-            for var in vars.values():
-                var.set_auto_maskandscale(False)
+    def chunking(self):
+        return 'contiguous'
 
-        # add missing dimensions
-        for dim in dims:
-            if dim not in vars:
-                if NETCDF_MODULE == NETCDF4:
-                    length = len(dims[dim])
-                else:
-                    length = dims[dim]
-                vars[dim] = np.arange(length)
+    def filters(self):
+        return None
 
-        # build dataset
-        name = os.path.split(filepath)[1]
-        self.dataset = DatasetType(
-            name, attributes=dict(NC_GLOBAL=attrs(self.fp)))
-        for dim in dims:
-            if dims[dim] is None:
-                self.dataset.attributes['DODS_EXTRA'] = {
-                    'Unlimited_Dimension': dim,
-                }
-                break
+    def get_var_chunk_cache(self):
+        raise NotImplementedError('get_var_chunk_cache is not implemented')
+        return
 
-        # add grids
-        grids = [var for var in vars if var not in dims]
-        for grid in grids:
-            self.dataset[grid] = GridType(grid, attrs(vars[grid]))
-            # add array
-            self.dataset[grid][grid] = BaseType(
-                grid, vars[grid], vars[grid].dimensions, attrs(vars[grid]))
-            # add maps
-            for dim in vars[grid].dimensions:
-                self.dataset[grid][dim] = BaseType(
-                    dim, vars[dim][:], None, attrs(vars[dim]))
+    def ncattrs(self):
+        return self._attributes
 
-        # add dims
-        for dim in dims:
-            self.dataset[dim] = BaseType(
-                dim, vars[dim][:], None, attrs(vars[dim]))
+    def getncattr(self, attr):
+        return self._attributes[attr]
 
-    def close(self):
-        """Close the NetCDF file."""
-        self.fp.close()
+    def __getattr__(self, name):
+        # from netcdf4-python
+        # if name in _private_atts, it is stored at the python
+        # level and not in the netCDF file.
+        if name.startswith('__') and name.endswith('__'):
+            # if __dict__ requested, return a dict with netCDF attributes.
+            if name == '__dict__':
+                names = self.ncattrs()
+                values = []
+                for name in names:
+                    values.append(self._attributes[name])
+                return OrderedDict(zip(names, values))
+            else:
+                raise AttributeError
+        else:
+            return self.getncattr(name)
+
+    def getValue(self):
+        return self[...]
+
+    def __array__(self):
+        return self[...]
+
+    def __getitem__(self, key):
+        with netcdf_file(self.filepath, 'r') as source:
+            return (np.asarray(source[self.path][key])
+                    .astype(self.dtype)) #.reshape(self._reshape))
+
+    def reshape(self, *args):
+        if len(args) > 1:
+            self._reshape = args
+        else:
+            self._reshape = args
+        return self
+
+    @property
+    def shape(self):
+        return self._shape
+
+    def __len__(self):
+        if not self.shape:
+            raise TypeError('len() of unsized object')
+        else:
+            return self.shape[0]
+
+    def _getdims(self):
+        return self.dimensions
 
 
 if __name__ == "__main__":
@@ -115,4 +201,4 @@ if __name__ == "__main__":
     from werkzeug.serving import run_simple
 
     application = NetCDFHandler(sys.argv[1])
-    run_simple('localhost', 8001, application, use_reloader=True)
+    run_simple('0.0.0.0', 8001, application, use_reloader=True)
